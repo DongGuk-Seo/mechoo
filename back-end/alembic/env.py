37d4b05392632e20ac147f6e70e94d2b8713d88c
DongGuk-Seo <failnxid@gmail.com>
2023-11-29 22:24:38 +0900

    feat: req & ignore update

--- back-end/alembic/env.py
+++ back-end/alembic/env.py
@@ -1,5 +1,13 @@
 import os
 
+from alembic import context
+from sqlalchemy import engine_from_config
+from logging.config import fileConfig
+
+config = context.config
+
+# fileConfig(config.config_file_name)
+
 def get_database():
     user = os.getenv("POSTGRES_USER")
     password = os.getenv("POSTGRES_PASSWORD")
