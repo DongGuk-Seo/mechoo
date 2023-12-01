47133735820a0dcf2455cacaef330325b3de9e4c
DongGuk-Seo <failnxid@gmail.com>
2023-12-01 23:49:19 +0900

    feat: revise user model

--- back-end/app/models/user.py
+++ back-end/app/models/user.py
@@ -1,22 +1,23 @@
 from typing import Union
 
 from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from app.db.engine import Base
 
 class User(Base):
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, index=True)
-    username = Column(String)
+    email = Column(String(64), index=True)
+    username = Column(String(64))
     password = Column(String)
 
 class UserDetail(Base):
     __tablename__ = "user_detail"
 
-    user_id = ForeignKey(User)
-    profile_image = Column(String)
-    profile_intro = Column(String)
-    phone_number = Column(String)
-    gender = Column(Boolean)
-    birth = Column(Date)
+    profile_image = Column(String, nullable=True)
+    profile_intro = Column(String, nullable=True)
+    phone_number = Column(String(13), nullable=True)
+    gender = Column(Boolean, nullable=True)
+    birth = Column(Date, nullable=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
