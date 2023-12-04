1531c1cf536cd2cad9fd576515554fa7a1adb411
DongGuk-Seo <failnxid@gmail.com>
2023-12-04 22:39:30 +0900

    feat: make crud for user & fix some models

--- back-end/app/models/user.py
+++ back-end/app/models/user.py
@@ -1,28 +1,32 @@
 from typing import Union
+from datetime import datetime
 
+from pydantic import BaseModel
 from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
-from db.engine import Base
+from sqlalchemy.orm import DeclarativeBase, Mapped ,relationship, mapped_column
+
+class Base(DeclarativeBase):
+    pass
 
 class User(Base):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    email = Column(String(64), index=True, nullable=False)
-    username = Column(String(64), nullable=False)
-    password = Column(String, nullable=False)
-    is_active = Column(Boolean, nullable=False, default=True)
-    created_at = Column(Date, nullable=False, autoincrement=True)
-    user_detail = relationship("UserDetail", back_populates="user")
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False, autoincrement=True)
+    email: Mapped[str] = mapped_column(String(64), index=True, nullable=False)
+    username: Mapped[str] = mapped_column(String(64), nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+    created_at: Mapped[datetime] = mapped_column(Date, nullable=False, default=datetime.now())
+    user_detail: Mapped["UserDetail"] = relationship("UserDetail", back_populates="user", cascade="all, delete-orphan")
 
 class UserDetail(Base):
     __tablename__ = "user_detail"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    profile_image = Column(String, nullable=True)
-    profile_intro = Column(String, nullable=True)
-    phone_number = Column(String(13), nullable=True)
-    gender = Column(Boolean, nullable=True)
-    birth = Column(Date, nullable=True)
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=False, index=True)
-    user = relationship("User", back_populates="user_detail")
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
+    profile_image: Mapped[str] = mapped_column(String, nullable=True)
+    profile_intro: Mapped[str] = mapped_column(String, nullable=True)
+    phone_number: Mapped[str] = mapped_column(String(13), nullable=True)
+    gender: Mapped[bool] = mapped_column(Boolean, nullable=True)
+    birth: Mapped[datetime] = mapped_column(Date, nullable=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"), nullable=False, index=True)
+    user: Mapped["User"] = relationship("User", back_populates="user_detail")
--- back-end/app/schemas/user.py
+++ back-end/app/schemas/user.py
@@ -1,10 +1,20 @@
+from typing import Optional
+from datetime import datetime
 from pydantic import BaseModel
 
 class UserBase(BaseModel):
     email: str
+    username: str
+    is_active: bool = True
+    created_at: datetime = datetime.now()
 
 class UserCreate(UserBase):
+    email: str
+    username: str
     password: str
+
+class UserUpdate(UserBase):
+    password: Optional[str] = None
 
 class User(UserBase):
     id: int
