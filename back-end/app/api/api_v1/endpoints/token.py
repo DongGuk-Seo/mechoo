f69d5985ac3e7543ad383a22095742724c1e12ff
DongGuk-Seo <failnxid@gmail.com>
2023-12-16 02:04:44 +0900

    feat: reissue token

--- back-end/app/api/api_v1/endpoints/token.py
+++ back-end/app/api/api_v1/endpoints/token.py
@@ -9,7 +9,17 @@ from core.security import create_token, valid_token
 router = APIRouter()
 
 @router.post("/reissue")
-def reissue_token(session: SessionDep, token_in: TokenBase) -> Optional[TokenOutput]:
+def reissue_token(request: Request,session: SessionDep, token_in: TokenBase) -> Optional[TokenOutput]:
     is_expired = valid_token(token_in.refresh_token)
-    if not 
-    token_model = token.get_user_by_refresh_token(session, token_in.refresh_token)
+    if not is_expired and request.client:
+        token_model = token.expire_token(db=session, db_obj=token.get_user_by_refresh_token(session, token_in.refresh_token))
+        tokens = create_token(token_model.user_id)
+        obj_in = TokenInput(
+            refresh_token=tokens.refresh_token, 
+            user_id=token_model.user_id,
+            location=request.client.host
+            )
+        token.create(db=session, obj_in=obj_in)
+        res = TokenOutput(refresh_token=tokens.refresh_token, access_token=tokens.access_token)
+        return res
+    return None
--- back-end/app/crud/crud_token.py
+++ back-end/app/crud/crud_token.py
@@ -7,7 +7,7 @@ from crud.base import CRUDBase
 from models.token import Token
 from schemas.token import TokenBase, TokenOutput, TokenInput, TokenUpdate
 
-class CRUDToken(CRUDBase[Token, TokenBase, TokenOutput]):
+class CRUDToken(CRUDBase[Token, TokenBase, TokenUpdate]):
     def get_user_by_refresh_token(self, db: Session, refresh_token: str) -> Token:
         token_model = db.query(Token).filter(Token.refresh_token == refresh_token).first()
         if token_model:
@@ -25,14 +25,8 @@ class CRUDToken(CRUDBase[Token, TokenBase, TokenOutput]):
         db.refresh(db_obj)
         return db_obj
 
-    def expire_token(self, db: Session, db_obj: Token, obj_in: Union[TokenUpdate, Dict[str, Any]]) -> Token:
-        if isinstance(obj_in, dict):
-            update_data = obj_in
-        else:
-            update_data = obj_in.dict(exclude_unset=True)
-        if not update_data["is_expired"]:
-            del update_data["is_expired"]
-            update_data["is_expired"] = True
+    def expire_token(self, db: Session, db_obj: Token) -> Token:
+        update_data = TokenUpdate(refresh_token=db_obj.refresh_token, is_expired=True)
         return super().update(db, db_obj=db_obj, obj_in=update_data)
 
 token = CRUDToken(Token)
