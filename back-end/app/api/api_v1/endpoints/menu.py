eff929b5585d715bac25bfdb002fc08f60778441
DongGuk-Seo <failnxid@gmail.com>
2023-12-26 22:24:40 +0900

    feat: update menu detail

--- back-end/app/api/api_v1/endpoints/menu.py
+++ back-end/app/api/api_v1/endpoints/menu.py
@@ -5,7 +5,7 @@ from crud import menu, menu_detail
 from api.deps import SessionDep
 from models.menu import Menu
 from schemas.menu import MenuCreate, MenuUpdate, MenuOutput
-from schemas.menu_detail import MenuDetailCreate, MenuDetailOutput
+from schemas.menu_detail import MenuDetailCreate, MenuDetailOutput, MenuDetailUpdate
 from core.utils import exception_400_already_exist, exception_404_not_found
 
 router = APIRouter()
@@ -29,5 +29,15 @@ async def create_menu_detail(session: SessionDep, menu_detail_in: MenuDetailCrea
         menu_detail.valid_is_exist(db=session,menu_id=menu_detail_in.menu_id)
         menu_detail_model = menu_detail.create(db=session, obj_in=menu_detail_in)
         return MenuDetailOutput(**menu_detail_model.__dict__) 
+    raise exception_404_not_found("존재하지 않는 메뉴 입니다.")
+
+@router.put("/detail")
+async def update_menu_detail(session: SessionDep, menu_detail_in: MenuDetailUpdate) -> MenuDetailOutput:
+    if menu.get_menu_by_id(db=session,id=menu_detail_in.menu_id):
+        menu_detail_model = menu_detail.get_menu_detail_by_menu_id(db=session, menu_id=menu_detail_in.menu_id)
+        if menu_detail_model:
+            new_menu_detail = menu_detail.update(db=session,db_obj=menu_detail_model, obj_in=menu_detail_in)
+            return MenuDetailOutput(**new_menu_detail.__dict__)
+        raise exception_404_not_found("존재하지 않는 메뉴 세부정보 입니다.")
     raise exception_404_not_found("존재하지 않는 메뉴 입니다.")
     
